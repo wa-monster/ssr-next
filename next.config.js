12d79c31bb8856360fba8a3b8529d92258067dee
yangjie <2037485086@qq.com>
2023-03-23 14:48:29 +0800

    feat(): 提交1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+// next.config.js
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
